ca02a925c1cc0cc9e63668c4b90bccc54242aae3
giusepperusso412 <russogiuseppe412@gmail.com>
2022-03-22 14:20:34 +0100

    sconto

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 const numeroKm = prompt("Quanti Km vuoi percorrere?");
 const numeroAnni = prompt("Quanti anni hai?");
 
+const costoKm = numeroKm * 0.21;
+const sconto20 = costoKm - (20 / 100);
+//const totale = costoKm - sconto20;
+
+if(numeroAnni<18){
+    if(costoKm - sconto20){
+        alert(`Costo biglietto è:${totale}€`);
+    }
+   
+}
+
+alert(`Costo biglietto è:${costoKm}€`);
