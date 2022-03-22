39016089e60e1a622aa950cc944a23837276ecfc
giusepperusso412 <russogiuseppe412@gmail.com>
2022-03-22 14:44:27 +0100

    fin

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const numeroKm = prompt("Quanti Km vuoi percorrere?");
 const numeroAnni = prompt("Quanti anni hai?");
 
-const costoKm = numeroKm * 0.21;
+const costoKm = (numeroKm * 0.21).toFixed(2);
 const sconto20 = 20 / 100;
-const totale = costoKm - (costoKm*sconto20);
+const totale = costoKm - (costoKm*sconto20).toFixed(2);
 const sconto40 = 40 / 100;
-const totale2 = costoKm - (costoKm * sconto40);
+const totale2 = costoKm - (costoKm * sconto40).toFixed(2);
 
 if(numeroAnni<18){
     alert(`Costo biglietto è:${totale}€`);
 } else if(numeroAnni>=65){
     alert(`Costo biglietto è:${totale2}€`);
+} else{
+    alert(`Costo biglietto è:${costoKm}€`);
 }
 
-alert(`Costo biglietto è:${costoKm}€`);
